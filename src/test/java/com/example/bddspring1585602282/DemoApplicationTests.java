bd41c59a69e2efd1bffc691214b0a17275c08a0b
Andrew Bayer <andrew.bayer@gmail.com>
2020-03-30 17:04:44 -0400

    Initial import

---
+++ src/test/java/com/example/bddspring1585602282/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bddspring1585602282;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
